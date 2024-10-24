9210b9ed37bdf6fd3b3573cae6d646bd0063ffa4
Gabriel Lagrota <95034068+LagrotaGabriel@users.noreply.github.com>
2024-10-23 22:33:10 -0300

    [FEAT-2] feat: Criado mapeamento da entidade CompraEntity

---
+++ src/main/java/br/com/compras/modules/compras/entity/CompraEntity.java
@@ -0,0 +1,50 @@
+package br.com.compras.modules.compras.entity;
+
+import br.com.compras.modules.cliente.entity.ClienteEntity;
+import br.com.compras.modules.produto.entity.ProdutoEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.Comment;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "TB_COMPRA")
+public class CompraEntity {
+
+    @Id
+    @Comment("Chave primária da compra - UUID")
+    @Column(name = "COD_COMPRA_CMP", nullable = false, updatable = false)
+    private UUID id;
+
+    @Comment("Data e hora que a compra foi cadastrada")
+    @Column(name = "LDT_DATAHORACADASTRO_CMP", nullable = false, updatable = false)
+    private LocalDateTime dataHoraCadastro;
+
+    @Comment("Quantidade de produtos adquiridos")
+    @Column(name = "INT_QUANTIDADE_MINIMA_PDT", nullable = false)
+    private Integer quantidade;
+
+    @Comment("Valor unitário da compra realizada")
+    @Column(name = "DCM_VALORUNITARIO_PDT", scale = 2, nullable = false)
+    private BigDecimal valorUnitario;
+
+    @ToString.Exclude
+    @Comment("Produto da compra")
+    @OneToOne(targetEntity = ProdutoEntity.class, fetch = FetchType.LAZY)
+    private ProdutoEntity produto;
+
+    @ToString.Exclude
+    @ManyToOne(fetch = FetchType.LAZY)
+    @Comment("Cliente que realizou a compra")
+    @JoinColumn(name = "COD_CLIENTE_CMP", referencedColumnName = "COD_CLIENTE_CLN", nullable = false, updatable = false)
+    private ClienteEntity cliente;
+}
