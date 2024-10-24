eb70bbed26900a9e5da1e700adccec0937b44650
Gabriel Lagrota <95034068+LagrotaGabriel@users.noreply.github.com>
2024-10-24 16:29:24 -0300

    [FEAT-15] refac: Atributo idCompra renomeado para id

--- src/main/java/br/com/compras/modules/compras/actions/cadastro/dto/response/CriaCompraResponse.java
+++ src/main/java/br/com/compras/modules/compras/actions/cadastro/dto/response/CriaCompraResponse.java
@@ -7,11 +7,12 @@ import java.util.UUID;
 
 @Getter
 @Setter
+@Builder
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
 public class CriaCompraResponse {
 
     @Parameter(name = "Id da compra", example = "74bcd515-655c-4def-8dc8-3329ccbdf62e")
-    private UUID idCompra;
+    private UUID id;
 }
