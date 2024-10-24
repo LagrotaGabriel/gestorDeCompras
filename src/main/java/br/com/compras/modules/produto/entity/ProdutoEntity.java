508c94f6b9e39f01381a691cef67a22448405e0c
Gabriel Lagrota <95034068+LagrotaGabriel@users.noreply.github.com>
2024-10-23 22:32:04 -0300

    [FEAT-2] feat: Criado mapeamento de entidade do Produto

---
+++ src/main/java/br/com/compras/modules/produto/entity/ProdutoEntity.java
@@ -0,0 +1,30 @@
+package br.com.compras.modules.produto.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+import org.hibernate.annotations.Comment;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "TB_PRODUTO")
+public class ProdutoEntity {
+
+    @Id
+    @Comment("Chave primária do produto - UUID")
+    @Column(name = "COD_PRODUTO_PDT", nullable = false, updatable = false)
+    private UUID id;
+
+    @Comment("Nome do produto")
+    @Column(name = "STR_NOME_PDT", unique = true, nullable = false, updatable = false, length = 60)
+    private String nome;
+}
