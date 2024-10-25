02dcf76aacb86665e6542e0b61f2712ae381609f
Gabriel Lagrota <95034068+LagrotaGabriel@users.noreply.github.com>
2024-10-25 00:30:00 -0300

    [FEAT-19] feat: Criado método responsável por implementar acesso a interface de pesquisa de compras no banco de dados da aplicação

--- src/main/java/br/com/compras/modules/compras/repository/dao/CompraDAO.java
+++ src/main/java/br/com/compras/modules/compras/repository/dao/CompraDAO.java
@@ -2,6 +2,8 @@ package br.com.compras.modules.compras.repository.dao;
 
 import br.com.compras.modules.compras.entity.CompraEntity;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 public interface CompraDAO {
@@ -13,5 +15,12 @@ public interface CompraDAO {
     Integer realizaContagemDaQuantidadeDeProdutosPorIdAdquiridosPeloCliente(
             UUID idProdutoAdquirido,
             UUID idClienteComprador
+    );
+
+    List<CompraEntity> realizaPesquisa(
+            String buscaPorCpfCliente,
+            String buscaPorNomeProduto,
+            LocalDateTime dataInicio,
+            LocalDateTime dataFim
     );
 }
